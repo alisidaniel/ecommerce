226799f7e6347043b21503156fad03272a64fd27
Daniel <alisidaniel@gmail.com>
2019-09-25 19:54:59 +0100

    git commit postgresql settings in heroku

--- djangoproject/djangoproject/settings/production.py
+++ djangoproject/djangoproject/settings/production.py
@@ -117,6 +117,10 @@ DATABASES ={
     }
 }
 
+import dj_database_url #Override an use postgresql Database in heroku
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
